2c1d4bce3eb3e7715bbe193f62e28f09c0f3dae0
Endermaiter <mpereznovoa@danielcastelao.org>
2023-01-12 14:42:00 +0100

    MainActivity2.kt con un intent enviando datos a la MainActivity.kt acabado (Se abren dos activities)

--- app/src/main/java/com/marcos/secondactivity/MainActivity.kt
+++ app/src/main/java/com/marcos/secondactivity/MainActivity.kt
@@ -1,21 +1,71 @@
 package com.marcos.secondactivity
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG_ACTIVITY = "ACTIVITYCODE"
+    val RESULTADO_1 = 1
+    val RESULTADO_2 = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var changeButton = findViewById<Button>(R.id.changeButton)
 
-        changeButton.setOnClickListener{
-            val intent = Intent(this,MainActivity2::class.java)
+        changeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
             intent.putExtra("clientes", 5)
             intent.putExtra("proveedores", 90)
-            startActivity(intent)
+
+
+            startActivityForResult(intent, RESULTADO_1)
+            startActivityForResult(intent, RESULTADO_2)
+
+
+
+
         }
     }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d(TAG_ACTIVITY, "Recibe el dato: ${data?.getStringExtra("saludo")}")
+        super.onActivityResult(requestCode, resultCode, data)
+        //difinimos un textView para visualizar el dato de la MainActivity2
+        val saludo = findViewById<TextView>(R.id.textViewSaludo)
+        val despedida = findViewById<TextView>(R.id.textViewAdios)
+
+        Log.d(TAG_ACTIVITY, "requestCode = $requestCode")
+        Log.d(TAG_ACTIVITY,"Activity.RESULT_OK = ${Activity.RESULT_OK}")
+
+        //si es OK
+        if (requestCode != Activity.RESULT_OK) {
+            Log.d(TAG_ACTIVITY, "Entra en el if(requestCode != Activity.RESULT_OK)")
+            //Hace diferentes cosas segun el requestCode
+            when (requestCode) {
+                RESULTADO_1 -> {
+                    Log.d(TAG_ACTIVITY, "requestCode = RESULTADO_1")
+                    if (data != null) {
+                        Log.d(TAG_ACTIVITY, "El intent no es null")
+                        saludo.text = data.getStringExtra("saludo")
+                    };}
+                RESULTADO_2 -> {
+                    Log.d(TAG_ACTIVITY, "requestCode = RESULTADO_2")
+                    if (data != null) {
+                        Log.d(TAG_ACTIVITY, "El intent no es null")
+                        despedida.text = data.getStringExtra("despedida")
+                    };}
+                else -> {}
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/marcos/secondactivity/MainActivity2.kt
+++ app/src/main/java/com/marcos/secondactivity/MainActivity2.kt
@@ -1,27 +1,49 @@
 package com.marcos.secondactivity
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    val TAG_ACTIVITY = "ACTIVITYCODE"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        val intent = getIntent()
+
         val changeButton = findViewById<Button>(R.id.changeButton2)
 
         changeButton.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+            //enviamos valores con setResult
+
+            //valor: Hola
+            intent.putExtra("saludo", "Hola")
+            Log.d(TAG_ACTIVITY, "Se añade el nuevo dato")
+            Log.d(TAG_ACTIVITY, "Activity.RESULT_OK = ${Activity.RESULT_OK}")
+
+            //valor: Adios
+            intent.putExtra("despedida", "Adios")
+            Log.d(TAG_ACTIVITY, "Se añade otro dato")
+
+            //enviamos los datos
+            setResult(Activity.RESULT_OK, intent)
+            Log.d(TAG_ACTIVITY, "Se envian los datos")
+
+            //Tras enviar los datos, cerramos la MainActivity2
+            finish()
         }
 
-        val intent = intent
 
+
+        //recibimos los valores y los mostramos
         val clientesValue = intent.getIntExtra("clientes",0)
         val proveedoresValue = intent.getIntExtra("proveedores",0)
         val numClientes = findViewById<TextView>(R.id.textViewClientes)
