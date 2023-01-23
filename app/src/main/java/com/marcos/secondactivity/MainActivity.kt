16fa41623277e4240258bf93278b2adaad2af624
Endermaiter <mpereznovoa@danielcastelao.org>
2023-01-23 10:25:45 +0100

    Telefono funciona correctamente

--- app/src/main/java/com/marcos/secondactivity/MainActivity.kt
+++ app/src/main/java/com/marcos/secondactivity/MainActivity.kt
@@ -1,39 +1,26 @@
 package com.marcos.secondactivity
 import android.content.Intent
-import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-
-    val RESULT_CAMERA = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var changeButton = findViewById<Button>(R.id.changeButton)
+        val changeButton = findViewById<Button>(R.id.changeButton)
+        val numeroTlf = findViewById<EditText>(R.id.textUrl)
 
         changeButton.setOnClickListener {
-            //Creamos un intent que inicia la camara cuando pulsamos el boton
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            //llamamos a la activity de la camara
-            startActivityForResult(intent, RESULT_CAMERA)
-        }
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        if (requestCode == RESULT_CAMERA && resultCode == RESULT_OK && data != null) {
-            //recogemos la imagen tomada en una variable de tipo Bitmap
-            val imageBitmap = data.extras!!.get("data") as Bitmap
-            //Le mostramos la foto sacada al usuario mediante el imageView
-            imageView.setImageBitmap(imageBitmap)
-
+            // creamos un intent que abre el telefono
+            val intent = Intent(Intent.ACTION_DIAL)
+            //le pasamos al intent el numero de tlf
+            intent.data = Uri.parse("tel:" + numeroTlf.text)
+            //ejecutamos la activity, la cual abrira el telefono con el numero dado
+            startActivity(intent)
         }
     }
 }
