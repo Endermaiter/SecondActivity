23af1f85353c8f9cf0d610160f735cd2ffd77f83
Endermaiter <mpereznovoa@danielcastelao.org>
2023-01-23 09:53:11 +0100

    Camara funcional. Se muestra en el imageView

--- app/src/main/java/com/marcos/secondactivity/MainActivity.kt
+++ app/src/main/java/com/marcos/secondactivity/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.marcos.secondactivity
-
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG_ACTIVITY = "ACTIVITYCODE"
-    val RESULTADO_1 = 1
-    val RESULTADO_2 = 2
+    val RESULT_CAMERA = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,51 +17,24 @@ class MainActivity : AppCompatActivity() {
         var changeButton = findViewById<Button>(R.id.changeButton)
 
         changeButton.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            intent.putExtra("clientes", 5)
-            intent.putExtra("proveedores", 90)
-
-
-            startActivityForResult(intent, RESULTADO_1)
-            startActivityForResult(intent, RESULTADO_2)
-
-
-
-
+            //Creamos un intent que inicia la camara cuando pulsamos el boton
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //llamamos a la activity del tlf.
+            startActivityForResult(intent, RESULT_CAMERA)
         }
     }
 
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        Log.d(TAG_ACTIVITY, "Recibe el dato: ${data?.getStringExtra("saludo")}")
         super.onActivityResult(requestCode, resultCode, data)
-        //difinimos un textView para visualizar el dato de la MainActivity2
-        val saludo = findViewById<TextView>(R.id.textViewSaludo)
-        val despedida = findViewById<TextView>(R.id.textViewAdios)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        if (requestCode == RESULT_CAMERA && resultCode == RESULT_OK && data != null) {
+            //recogemos la imagen tomada en una variable de tipo Bitmap
+            val imageBitmap = data.extras!!.get("data") as Bitmap
+            //Le mostramos la foto sacada al usuario mediante el imageView
+            imageView.setImageBitmap(imageBitmap)
 
-        Log.d(TAG_ACTIVITY, "requestCode = $requestCode")
-        Log.d(TAG_ACTIVITY,"Activity.RESULT_OK = ${Activity.RESULT_OK}")
-
-        //si es OK
-        if (requestCode != Activity.RESULT_OK) {
-            Log.d(TAG_ACTIVITY, "Entra en el if(requestCode != Activity.RESULT_OK)")
-            //Hace diferentes cosas segun el requestCode
-            when (requestCode) {
-                RESULTADO_1 -> {
-                    Log.d(TAG_ACTIVITY, "requestCode = RESULTADO_1")
-                    if (data != null) {
-                        Log.d(TAG_ACTIVITY, "El intent no es null")
-                        saludo.text = data.getStringExtra("saludo")
-                    };}
-                RESULTADO_2 -> {
-                    Log.d(TAG_ACTIVITY, "requestCode = RESULTADO_2")
-                    if (data != null) {
-                        Log.d(TAG_ACTIVITY, "El intent no es null")
-                        despedida.text = data.getStringExtra("despedida")
-                    };}
-                else -> {}
-            }
         }
     }
-
 }
+
